3d36f6e71c487b6e8b122efa463a9f79ec3ab6af
fardian <fardianputra0@gmail.com>
2024-05-07 10:48:12 +0700

    update vendor

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Buat Postingan</title>
-    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -28,8 +27,7 @@
         </form>
     </div>
 
-    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}">
     </script>
 </body>
 
